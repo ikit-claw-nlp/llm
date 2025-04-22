b730b7244ec36447aacff21d94f6498b256ae00f
Yuxun Lu <yuxun.a.lu@rakuten.com>
2025-04-22 06:12:05 +0000

    code updates

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -1,3 +1,6 @@
+import os
+# use this to enable parallel training.
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
 from tokenizers import SentencePieceBPETokenizer
 import glob
 """
@@ -6,6 +9,10 @@ Reference:
 - https://github.com/huggingface/tokenizers
 - The [source code](https://github.com/huggingface/tokenizers/blob/f2ec3b239b0a7a9866b01ec5cbd4d44243a40a16/bindings/python/py_src/tokenizers/implementations/sentencepiece_bpe.py#L78) of SentencePieceTokenizer
 - https://discuss.huggingface.co/t/training-sentencepiece-from-scratch/3477
+
+In addition, consider convert sentencepiece (the library from Google) format to tokenizer format in the future:
+the sentencepiece library: https://github.com/google/sentencepiece/blob/master/README.md
+useful article: https://github.com/huggingface/tokenizers/issues/1311
 """
 if __name__ == "__main__":
     corpus_files = glob.glob("./data/text/*.txt")
