4cf76ffb82701a27823e57c1a4e7931a54be9551
Yuxun Lu <yuxun.a.lu@rakuten.com>
2025-04-24 03:43:35 +0000

    code updates.

--- spm_train_tokenizer.py
+++ spm_train_tokenizer.py
@@ -4,11 +4,14 @@ if __name__ == "__main__":
     spm.SentencePieceTrainer.train(
     # cf. https://github.com/google/sentencepiece#train-sentencepiece-model
     # it says the input must be one-sentence-per-line text.
+    # cf. the option user_defined_symbols to understand how to customize symbols.
     input = "data/full_text/sentence_tokenized_corpus.corpus", 
     model_prefix = "ikit_spm",
     vocab_size=50000,
     character_coverage=0.9995,
-    num_threads=32,
+    num_threads=64,
+    input_sentence_size=500000,
+    shuffle_input_sentence=True,
     unk_id=0,
     bos_id=1,
     eos_id=2,
